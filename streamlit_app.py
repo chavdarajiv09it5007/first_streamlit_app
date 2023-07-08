f7a84d35174de22f5c21bad26df54ff26be0d028
chavdarajiv09it5007 <138447934+chavdarajiv09it5007@users.noreply.github.com>
2023-07-08 21:59:18 +0530

    Update streamlit_app.py
    Get All the Rows, Not Just One

--- streamlit_app.py
+++ streamlit_app.py
@@ -128,11 +128,20 @@ import snowflake.connector
 
 
 #Let's Change the Streamlit Components to Make Things Look a Little Nicer
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("select * from fruit_load_list")
+#my_data_row = my_cur.fetchone()
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_row)
+
+
+
+#Oops! Let's Get All the Rows, Not Just One
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
-
+streamlit.dataframe(my_data_rows)
 
