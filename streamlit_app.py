684d51b0733fcce2b8e52c7644299d93601624fd
chavdarajiv09it5007 <138447934+chavdarajiv09it5007@users.noreply.github.com>
2023-07-08 19:56:27 +0530

    Update streamlit_app.py
    added code to fetch via request

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+
+
+#chapter 9
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
