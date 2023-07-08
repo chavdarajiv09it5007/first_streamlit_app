f6cefa419b1d3a2fc32f3baf834d39cf9f116f8d
chavdarajiv09it5007 <138447934+chavdarajiv09it5007@users.noreply.github.com>
2023-07-08 23:15:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -154,18 +154,17 @@ except URLError as e:
 
 
 #Oops! Let's Get All the Rows, Not Just One
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+def get_fruit_load_list():
+    with my_cnx.cursor as my_cur:
+        #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        my_cur.execute("select * from fruit_load_list")
+        return my_data_rows
 
 
 # Add A Second Text Entry Box? 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor as my_cur:
-        my_cur.execute("Insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('" + ???? + "')")
+        my_cur.execute("Insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
         return ('Thanks for adding ', new_fruit)
 
 add_my_fruit = streamlit.text_input('What fruit would you like add?')
