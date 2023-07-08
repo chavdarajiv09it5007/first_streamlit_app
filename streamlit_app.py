7208d3529f70eae132517f7cc3eb086f4f4907dd
chavdarajiv09it5007 <138447934+chavdarajiv09it5007@users.noreply.github.com>
2023-07-08 23:04:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -165,7 +165,7 @@ streamlit.dataframe(my_data_rows)
 # Add A Second Text Entry Box? 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor as my_cur:
-        my_cur.execute("Insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('" + ???? + "')")
+        my_cur.execute("Insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('" + jackfruit + "')")
         return ('Thanks for adding ', new_fruit)
 
 add_my_fruit = streamlit.text_input('What fruit would you like add?')
@@ -174,4 +174,9 @@ if streamlit.button('Add a Fruit to the List:'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
 
+if streamlit.button('Get Fruit List:'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    my_cnx.close()
+    streamlit.dataframe(my_data_rows)
 
