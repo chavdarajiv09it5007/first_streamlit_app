36d9eceaf16920c6138bf63ba56a10c6602f6231
chavdarajiv09it5007 <138447934+chavdarajiv09it5007@users.noreply.github.com>
2023-07-08 22:24:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,14 +97,19 @@ streamlit.dataframe(fruits_to_show)
 # Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
 #import requests
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# take the jsoan version and normalize it 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# writes the normalise dta to scerrn
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  ####streamlit.write('The user entered ', fruit_choice)
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information.')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # take the jsoan version and normalize it 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # writes the normalise dta to scerrn
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 
 
