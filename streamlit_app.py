7d17981d3ca83a4f40643da25db02c58bc45ea73
chavdarajiv09it5007 <138447934+chavdarajiv09it5007@users.noreply.github.com>
2023-07-08 22:15:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -41,7 +46,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 #read csv,make it user interactive with dropdown name,prepopulate dropdown and show fruits on ui with filter applied
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
@@ -90,11 +95,11 @@ streamlit.dataframe(fruits_to_show)
 
 
 # Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
+#import requests
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # take the jsoan version and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -106,7 +111,8 @@ streamlit.dataframe(fruityvice_normalized)
 
 #chater 12 badge2
 #Let's Set Up Streamlit to Work with Snowflake
-import snowflake.connector
+streamlit.stop()  #dot run anything past here until we troubleshoot
+#import snowflake.connector
 
 #Let's Query Our Trial Account Metadata 
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
